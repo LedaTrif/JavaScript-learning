29b0154189cd0b5486d8aa16017a8b7315bf7c78
Leda <113000102+LedaTrif@users.noreply.github.com>
2022-11-23 11:45:11 +0100

    folders

---
+++ Basic Syntax, Conditional Statements and Loops - Lab	/ifElse.js
@@ -0,0 +1,20 @@
+function language(country) {
+    if (country=='England') {
+        console.log(`English`);
+    }
+    
+    else if (country=='Spain') {
+        console.log(`Spanish`);
+    }
+    else {
+      
+        console.log(`unknown`);
+    }
+}
+   
+
+   
+
+language("Spain");
+language("England");
+language("Bulgaria");
---
+++ Basic Syntax, Conditional Statements and Loops - Lab	/whileAndFor.js
@@ -0,0 +1,14 @@
+function solve(){
+    /*let a=1;
+    while(a<=5) {
+        console.log(a);
+        a=a+1;
+    }*/
+
+    for(let a=1; a<=5; a=a+1) {   //a++ ---> a=a+1
+console.log(a);
+    }
+    
+
+}
+solve(3);
