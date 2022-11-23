68dca14d21a00b4a60364ee4744b4eadb9042c35
Leda <113000102+LedaTrif@users.noreply.github.com>
2022-11-23 09:20:41 +0100

    updates

---
+++ forWhileNPlus3.js
@@ -0,0 +1,19 @@
+function solve() {
+
+    /*let n=3;
+    while (n<=100) {
+        console.log(n);
+        n+=3;
+    }
+}*/
+
+
+
+
+//for
+for (let n=3; n<=100; n+=3) {
+    console.log(n);
+}
+}
+
+solve();
