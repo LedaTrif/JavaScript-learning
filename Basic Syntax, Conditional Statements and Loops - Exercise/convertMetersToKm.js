a01cadb5cc1a5887b64a8e47ac8a248871a34aaa
Leda <113000102+LedaTrif@users.noreply.github.com>
2022-11-29 07:17:12 +0100

    update

---
+++ Basic Syntax, Conditional Statements and Loops - Exercise/convertMetersToKm.js
@@ -0,0 +1,6 @@
+function convertMetersToKm(input) {
+    console.log((input/1000).toFixed(2));
+}
+
+convertMetersToKm(1852);
+convertMetersToKm(798);
---
+++ Basic Syntax, Conditional Statements and Loops - Exercise/poundsToDollars.js
@@ -0,0 +1,6 @@
+function exchange(rate) {
+    let converted=(rate*1.31).toFixed(3);
+    console.log(converted);
+}
+
+exchange(80);
