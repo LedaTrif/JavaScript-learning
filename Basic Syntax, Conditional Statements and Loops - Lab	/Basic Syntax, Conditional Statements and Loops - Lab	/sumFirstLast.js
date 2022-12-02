407ba6abf3538a5d4e78a5b350907279d430e013
Leda <113000102+LedaTrif@users.noreply.github.com>
2022-12-02 07:32:47 +0100

    update

---
+++ Basic Syntax, Conditional Statements and Loops - Lab	/Basic Syntax, Conditional Statements and Loops - Lab	/sumFirstLast.js
@@ -0,0 +1,10 @@
+function sum(numbers) {
+
+    // let first= numbers[0];
+    let sum= numbers[0]+numbers[numbers.length-1];
+    //  let sum= first + last;
+    
+        console.log(sum);
+}
+
+sum([20,30,40]);
